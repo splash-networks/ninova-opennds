08adb81e06ee54489ec5e272c393a26524fb8037
Nasir Hafeez <38311694+nasirhafeez@users.noreply.github.com>
2024-04-01 16:23:04 +0500

    initial commit

--- public/verify.php
+++ public/verify.php
@@ -30,13 +30,41 @@ if (!isset($_POST['verify'])) {
 
   mysqli_query($con, "INSERT INTO `$table_name` (`phone`, `otp`, `valid`, `created_at`) VALUES ('$phone', '$otp', 1, NOW())");
 
-//  $message = $twilio->messages
-//    ->create($_SESSION["phone"], // to
-//      array(
-//        "from" => "+16562205453",
-//        "body" => "Twilio Test"
-//      )
-//    );
+  $curl = curl_init();
+
+  $data = array(
+    "type" => 1,
+    "sendingType" => 0,
+    "title" => "X tarihli tekil test",
+    "content" => "Merhaba doğrulama kodunuz : " . $otp,
+    "number" => $phone,
+    "encoding" => 0,
+    "sender" => "MERPABILGI",
+    "periodicSettings" => null,
+    "sendingDate" => null,
+    "validity" => 60,
+    "pushSettings" => null
+  );
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'http://smslogin.nac.com.tr:9587/sms/create',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => json_encode($data),
+    CURLOPT_HTTPHEADER => array(
+      'Content-Type: application/json',
+      'Authorization: Basic ' . $_SERVER['AUTH']
+    ),
+  ));
+
+  $response = curl_exec($curl);
+
+  curl_close($curl);
 } else {
   $_SESSION['code'] = trim($_POST['code']);
 
